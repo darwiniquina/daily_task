ce68ad921754ec1da4f42ad6210d32bfb1ab9d9b
darwiniquina <darwiniquina@gmail.com>
2025-12-30 21:20:44 +0800

    feat: tooltip shadn cn ui with toop tip wrapper

---
+++ src/components/ui/tooltip/TooltipWrapper.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from '@/components/ui/tooltip'
+
+interface Props {
+    text?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <TooltipProvider>
+        <Tooltip>
+            <TooltipTrigger as-child>
+                <slot />
+            </TooltipTrigger>
+
+            <TooltipContent v-if="text">
+                <p>{{ text }}</p>
+            </TooltipContent>
+
+            <slot name="content" v-else />
+        </Tooltip>
+    </TooltipProvider>
+</template>
